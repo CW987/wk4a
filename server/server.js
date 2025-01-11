c5c0ca080ad78808b7acf1ff29f4d2e0c0ef4202
Curtis Witter <inward-panting0f@icloud.com>
2025-01-11 18:14:13 +0000

    Link with Supabase, started seed and server

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 // import packages
+import pg from 'pg';
+import dotenv from 'dotenv';
 // start or configure packages
+dotenv.config();
 // tell server to use JSON
 // set up a port for the server by listening... 
 // setup database pool
